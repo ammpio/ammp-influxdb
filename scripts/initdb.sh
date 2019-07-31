89ae0fcd1d87f888209b6f03c110c112dea4ae7c
Svet Bajlekov <tomatoman@gmail.com>
2019-07-31 23:43:23 +0200

    Sleep 2 seconds before attempting to create database

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-set -e
+#set -e
 
 export DBNAME="db"
 export RETENTION="365d"
 export CREATE_DB_QUERY="CREATE DATABASE $DBNAME WITH DURATION $RETENTION"
+
+sleep 2
 
 $SNAP/bin/influx -execute "$CREATE_DB_QUERY"
