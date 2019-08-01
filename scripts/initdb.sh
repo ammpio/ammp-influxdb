a9c56dc2c2d2252e4c0de9ef12942ab5e5ad542b
Svet Bajlekov <tomatoman@gmail.com>
2019-08-01 23:26:21 +0200

    Fix databse initialization script

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -9,9 +9,10 @@ export INIT_QUERY="CREATE DATABASE $DBNAME WITH DURATION $RETENTION"
 
 # Wait for Influx server to come up properly
 for i in {30..0}; do
-    if $INFLUX_CMD "$INIT_QUERY" &> /dev/null; then
+    if $INFLUX_CMD "$INIT_QUERY"; then
+        echo 'Initialized database'
         break
     fi
-    echo 'InfluxDB init process in progress...'
+    echo 'Waiting for InfluxDB to become available...'
     sleep 1
 done
