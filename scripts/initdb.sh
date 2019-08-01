36c5065d379c88481c9831c2afe2d269627d47df
Svet Bajlekov <tomatoman@gmail.com>
2019-08-01 14:14:00 +0200

    Better init

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-#set -e
+set -e
+
+export INFLUX_CMD="$SNAP/bin/influx -execute "
 
 export DBNAME="db"
 export RETENTION="365d"
-export CREATE_DB_QUERY="CREATE DATABASE $DBNAME WITH DURATION $RETENTION"
+export INIT_QUERY="CREATE DATABASE $DBNAME WITH DURATION $RETENTION"
 
-sleep 2
-
-$SNAP/bin/influx -execute "$CREATE_DB_QUERY"
+# Wait for Influx server to come up properly
+for i in {30..0}; do
+    if $INFLUX_CMD "$INIT_QUERY" &> /dev/null; then
+        break
+    fi
+    echo 'InfluxDB init process in progress...'
+    sleep 1
+done
