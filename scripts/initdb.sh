5e292a8d26bb2188db35bc5abc445e2c3f6dccbe
Svet Bajlekov <tomatoman@gmail.com>
2019-08-02 00:29:31 +0200

    Update initialization script

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -8,11 +8,10 @@ export RETENTION="365d"
 export INIT_QUERY="CREATE DATABASE $DBNAME WITH DURATION $RETENTION"
 
 # Wait for Influx server to come up properly
-for i in {30..0}; do
-    if $INFLUX_CMD "$INIT_QUERY"; then
-        echo 'Initialized database'
-        break
-    fi
+while ! $INFLUX_CMD "$INIT_QUERY"
+do
     echo 'Waiting for InfluxDB to become available...'
     sleep 1
 done
+
+echo 'Initialized database'
